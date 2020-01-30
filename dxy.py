910ac8386dee9b7c1deae114085fa5b2ca42336e
xhn16729 <xiong_haonan@163.com>
2020-01-30 20:38:54 +0800

    Use new API for bot area command

--- dxy.py
+++ dxy.py
@@ -10,38 +10,47 @@ from lxml import etree
 
 def area():
     ans = ''
-    url = "https://3g.dxy.cn/newh5/view/pneumonia_peopleapp?from=timeline&isappinstalled=0"
-    request_headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'}
-    html = requests.get(url, headers=request_headers)
+    overall_url = "https://lab.isaaclin.cn/nCoV/api/overall"
+    html = requests.get(overall_url)
     html.encoding = 'utf-8'
-    content = etree.HTML(html.text)
-    number = content.xpath('//*[@id="getAreaStat"]')
-    data_time = content.xpath('//*[@id="root"]/div/div[3]/div[1]/p[1]/span')
-    nation_confirmedCount = content.xpath('//*[@id="root"]/div/div[3]/div[1]/p[2]/span/span[1]/span')
-    nation_suspectedCount = content.xpath('//*[@id="root"]/div/div[3]/div[1]/p[2]/span/span[2]/span')
-    nation_deadCount = content.xpath('//*[@id="root"]/div/div[3]/div[1]/p[2]/span/span[3]/span')
-    nation_curedCount = content.xpath('//*[@id="root"]/div/div[3]/div[1]/p[2]/span/span[4]/span')
-    data = number[0].text[27:-11]
+    data = json.loads(html.text)
+    nation_confirmedCount = data['results'][0]['confirmedCount']
+    nation_suspectedCount = data['results'][0]['suspectedCount']
+    nation_deadCount = data['results'][0]['deadCount']
+    nation_curedCount = data['results'][0]['curedCount']
+    _time = time.strftime('%Y-%m-%d %H:%M', time.localtime())
 
-    with open('content.html', 'w') as f:
-        f.write(html.text)
-        f.close()
-    with open('number.json', 'w') as f:
-        f.write(data)
-        f.close()
+    area_url = "https://lab.isaaclin.cn/nCoV/api/area"
+    html = requests.get(area_url)
+    html.encoding = 'utf-8'
+    data = json.loads(html.text)
 
-    data = json.loads(data)
+    ans += '{0:^6}\t确诊:{1:^6}\t疑似:{2:^6}\t治愈:{3:^6}\t死亡:{4:^6}'.format('全国', nation_confirmedCount, nation_suspectedCount, nation_curedCount, nation_deadCount)
 
-    print(data_time[0].text)
-    #print('{0:^6}\t确诊:{1:^6}\t疑似:{2:^6}\t治愈:{3:^6}\t死亡:{4:^6}'.format('全国', nation_confirmedCount[0].text, nation_suspectedCount[0].text, nation_curedCount[0].text, nation_deadCount[0].text))
-    ans += '{0:^6}\t确:{1:^6}\t疑:{2:^6}\t愈:{3:^6}\t死:{4:^6}'.format('全国', nation_confirmedCount[0].text, nation_suspectedCount[0].text, nation_curedCount[0].text, nation_deadCount[0].text) + '\n'
-    for i in data:
-        provinceShortName = i['provinceShortName']
-        confirmedCount = i['confirmedCount']
-        suspectedCount = i['suspectedCount']
-        curedCount = i['curedCount']
-        deadCount = i['deadCount']
-        #print('省份:{0:^6}\t确诊:{1:^6}\t疑似:{2:^6}\t治愈:{3:^6}\t死亡:{4:^6}'.format(provinceShortName, confirmedCount, suspectedCount, curedCount, deadCount))
-        ans += '{0:^6}\t确:{1:^6}\t疑:{2:^6}\t愈:{3:^6}\t死:{4:^6}'.format(provinceShortName, confirmedCount, suspectedCount, curedCount, deadCount) + '\n'
+    provinceShortName = []
+    confirmedCount = []
+    suspectedCount = []
+    curedCount = []
+    deadCount = []
+
+    for i in data['results']:
+        if i['country'] == '中国':
+            provinceShortName.append(i['provinceShortName'])
+            confirmedCount.append(i['confirmedCount'])
+            suspectedCount.append(i['suspectedCount'])
+            curedCount.append(i['curedCount'])
+            deadCount.append(i['deadCount'])
+
+    for i in range(0, len(confirmedCount) - 1):
+        for j in range(i + 1, len(confirmedCount)):
+            if confirmedCount[i] < confirmedCount[j]:
+                confirmedCount[i], confirmedCount[j] = confirmedCount[j], confirmedCount[i]
+                provinceShortName[i], provinceShortName[j] = provinceShortName[j], provinceShortName[i]
+                suspectedCount[i], suspectedCount[j] = suspectedCount[j], suspectedCount[i]
+                curedCount[i], curedCount[j] = curedCount[j], curedCount[i]
+                deadCount[i], deadCount[j] = deadCount[j], deadCount[i]
+
+    for i in range(0, len(confirmedCount)):
+        ans += '{0:^6}\t确诊:{1:^6}\t疑似:{2:^6}\t治愈:{3:^6}\t死亡:{4:^6}'.format(provinceShortName[i], confirmedCount[i], suspectedCount[i], curedCount[i], deadCount[i])
 
     return ans
