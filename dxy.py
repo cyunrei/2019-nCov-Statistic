0ba75354a99a9bcb8d1e4511d6e5707a0870db7a
xhn16729 <xiong_haonan@163.com>
2020-01-30 20:43:20 +0800

    Add global stats

--- dxy.py
+++ dxy.py
@@ -10,6 +10,7 @@ from lxml import etree
 
 def area():
     ans = ''
+    
     overall_url = "https://lab.isaaclin.cn/nCoV/api/overall"
     html = requests.get(overall_url)
     html.encoding = 'utf-8'
@@ -35,6 +36,34 @@ def area():
 
     for i in data['results']:
         if i['country'] == '中国':
+            provinceShortName.append(i['provinceShortName'])
+            confirmedCount.append(i['confirmedCount'])
+            suspectedCount.append(i['suspectedCount'])
+            curedCount.append(i['curedCount'])
+            deadCount.append(i['deadCount'])
+
+    for i in range(0, len(confirmedCount) - 1):
+        for j in range(i + 1, len(confirmedCount)):
+            if confirmedCount[i] < confirmedCount[j]:
+                confirmedCount[i], confirmedCount[j] = confirmedCount[j], confirmedCount[i]
+                provinceShortName[i], provinceShortName[j] = provinceShortName[j], provinceShortName[i]
+                suspectedCount[i], suspectedCount[j] = suspectedCount[j], suspectedCount[i]
+                curedCount[i], curedCount[j] = curedCount[j], curedCount[i]
+                deadCount[i], deadCount[j] = deadCount[j], deadCount[i]
+
+    for i in range(0, len(confirmedCount)):
+        ans += '{0:^6}\t确诊:{1:^6}\t疑似:{2:^6}\t治愈:{3:^6}\t死亡:{4:^6}'.format(provinceShortName[i], confirmedCount[i], suspectedCount[i], curedCount[i], deadCount[i])
+
+    provinceShortName = []
+    confirmedCount = []
+    suspectedCount = []
+    curedCount = []
+    deadCount = []
+
+    ans += ('===全球===')
+
+    for i in data['results']:
+        if i['country'] != '中国':
             provinceShortName.append(i['provinceShortName'])
             confirmedCount.append(i['confirmedCount'])
             suspectedCount.append(i['suspectedCount'])
