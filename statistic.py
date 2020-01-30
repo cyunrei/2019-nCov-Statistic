8b1adca2d56961d6f61ee713625c2780e2d86d89
xhn16729 <xiong_haonan@163.com>
2020-01-30 14:55:17 +0800

    Add some annotate

--- statistic.py
+++ statistic.py
@@ -9,11 +9,13 @@ from data import suspected
 from data import cured
 from data import confirmed
 
+# Define color
 confirmed_color = '#F74C31'
 suspected_color = '#F78207'
 dead_color = '#5D7092'
 cured_color = '#28B7A3'
 
+# Setup parameters
 plt.rcParams['font.family'] = ['Microsoft YaHei']
 plt.rcParams['savefig.format'] = 'png'
 plt.rcParams['figure.figsize'] = (16.0, 8.0)
@@ -23,13 +25,18 @@ url = "https://3g.dxy.cn/newh5/view/pneumonia_peopleapp?from=timeline&isappinsta
 request_headers = {
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) '
                   'Chrome/79.0.3945.130 Safari/537.36'}
+
+# Send request
 response = requests.get(url, headers=request_headers)
 response.encoding = 'utf-8'
 html = etree.HTML(response.text)
 
+# Pick data
 area_stat = html.xpath('//*[@id="getAreaStat"]')
 statistics = html.xpath('//*[@id="getStatisticsService"]')
 _time = time.strftime('20%y-%m-%d', time.localtime())
+
+# Save data into files
 data_list = []
 with open('./data/statistics (' + _time + ').json', 'w') as f:
     f.write(statistics[0].text[36:-11])
@@ -41,10 +48,10 @@ data_list.append(data_1['suspectedCount'])
 data_list.append(data_1['curedCount'])
 data_list.append(data_1['deadCount'])
 
+# Draw graph
 f1 = plt.subplot(2, 1, 1)
 f2 = plt.subplot(2, 1, 2)
 plt.sca(f1)
-
 plt.plot(list(confirmed.keys()), list(confirmed.values()),
          marker='o', label='确诊', color=confirmed_color, linewidth=3)
 plt.plot(list(suspected.keys()), list(suspected.values()),
@@ -60,11 +67,15 @@ title1 = '2019-nCov 全国趋势图 ' + _time
 plt.title('2019-nCov 全国疫情死亡/治愈累计趋势图 2019-01-11 至 ' + _time)
 plt.legend()
 plt.savefig('./view/{0}.png'.format(title1))
+
+# Save data into files
 with open('./data/area_stat (' + _time + ').json', 'w') as f:
     f.write(area_stat[0].text[27:-11])
     f.close()
 with open('./data/area_stat (' + _time + ').json', 'r') as f:
     data_2 = json.load(f)
+
+# Draw graph
 provinceShortName = []
 confirmedCount = []
 curedCount = []
